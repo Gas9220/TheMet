0f37e92114d22ed1286b14a726d6c21cdd13da18
Gaspare <Gas9220@users.noreply.github.com>
2023-12-19 11:59:38 +0100

    Initial Commit

---
+++ TheMet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TheMet
+//
+//  Created by Gaspare Monte on 19/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheMet/TheMetApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMetApp.swift
+//  TheMet
+//
+//  Created by Gaspare Monte on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
