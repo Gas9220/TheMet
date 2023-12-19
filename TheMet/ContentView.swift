836b50414e1da92800464e4e55fdcb1ab6627d90
Gaspare <Gas9220@users.noreply.github.com>
2023-12-19 15:24:44 +0100

    feat: include SafariView in content view navigation link

--- TheMet/ContentView.swift
+++ TheMet/ContentView.swift
@@ -13,9 +13,17 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List(store.objects, id: \.objectID) { object in
-                NavigationLink(object.title) {
-                    ObjectView(object: object)
+                NavigationLink {
+                    SafariView(url: URL(string: object.objectURL)!)
+                } label: {
+                    HStack {
+                        Text(object.title)
+                        Spacer()
+                        Image(systemName: "rectangle.portrait.and.arrow.right.fill")
+                            .font(.footnote)
+                    }
                 }
+
             }
             .navigationTitle("The Met")
         }
