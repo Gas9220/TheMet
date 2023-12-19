f89cf233807bb560f80e64eadd8e021f56d7579d
Gaspare <Gas9220@users.noreply.github.com>
2023-12-19 12:23:36 +0100

    feat: create object list and detail navigation

--- TheMet/ContentView.swift
+++ TheMet/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var store = TheMetStore()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(store.objects, id: \.objectID) { object in
+                NavigationLink(object.title) {
+                    ObjectView(object: object)
+                }
+            }
+            .navigationTitle("The Met")
         }
-        .padding()
     }
 }
 
