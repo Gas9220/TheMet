34877e59add08c4757d32ac77c43ab3725405677
Gaspare <Gas9220@users.noreply.github.com>
2023-12-19 12:17:24 +0100

    feat: add Color extension

---
+++ TheMet/ColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  TheMet
+//
+//  Created by Gaspare Monte on 19/12/23.
+//
+
+import SwiftUI
+
+extension Color {
+  // Add app colors to standard colors
+  static let metBg = Color("met-background")
+  static let metFg = Color("met-foreground")
+}
