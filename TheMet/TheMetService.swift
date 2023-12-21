2883ffa79c6023411e3a1a7604b9da1ffff8f960
Gaspare <Gas9220@users.noreply.github.com>
2023-12-21 09:22:21 +0100

    feat: add URLComponents extension

---
+++ TheMet/TheMetService.swift
@@ -0,0 +1,8 @@
+//
+//  TheMetService.swift
+//  TheMet
+//
+//  Created by Gaspare Monte on 21/12/23.
+//
+
+import Foundation
---
+++ TheMet/URLComponentsExtension.swift
@@ -0,0 +1,18 @@
+//
+//  URLComponentsExtension.swift
+//  TheMet
+//
+//  Created by Gaspare Monte on 19/12/23.
+//
+
+import Foundation
+
+public extension URLComponents {
+  /// Maps a dictionary into `[URLQueryItem]` then assigns it to the
+  /// `queryItems` property of this `URLComponents` instance.
+  /// From [Alfian Losari's blog.](https://www.alfianlosari.com/posts/building-safe-url-in-swift-using-urlcomponents-and-urlqueryitem/)
+  /// - Parameter parameters: Dictionary of query parameter names and values
+  mutating func setQueryItems(with parameters: [String: String]) {
+    self.queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+  }
+}
